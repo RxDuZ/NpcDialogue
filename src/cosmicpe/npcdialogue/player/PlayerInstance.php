8a248f68e333b78dd4ef6c0c6d0c814a75fe235e
RxDuZ <Devilmczgamerofc13@gmail.com>
2025-03-04 09:56:04 -0600

    Update PlayerInstance.php
    protocol changes.

--- src/cosmicpe/npcdialogue/player/PlayerInstance.php
+++ src/cosmicpe/npcdialogue/player/PlayerInstance.php
@@ -24,7 +24,8 @@ use function array_map;
 use function json_encode;
 use const JSON_THROW_ON_ERROR;
 
-final class PlayerInstance{
+final class PlayerInstance
+{
 
 	private bool $modify_abilities = false;
 	private ?PlayerNpcDialogueInfo $current_dialogue = null;
@@ -34,38 +35,41 @@ final class PlayerInstance{
 		readonly private PlayerManager $manager,
 		readonly private Player $player,
 		readonly private Logger $logger
-	){}
+	) {}
 
-	public function destroy() : void{
+	public function destroy(): void
+	{
 		$dialogue = $this->getCurrentDialogue();
-		if($dialogue !== null){
+		if ($dialogue !== null) {
 			$dialogue->onPlayerDisconnect($this->player);
 			$this->removeCurrentDialogue();
 		}
 	}
 
-	public function tick() : bool{
-		if($this->current_dialogue === null || $this->current_dialogue->status === PlayerNpcDialogueInfo::STATUS_CLOSED || $this->current_dialogue->dialogue !== NullNpcDialogue::instance()){
+	public function tick(): bool
+	{
+		if ($this->current_dialogue === null || $this->current_dialogue->status === PlayerNpcDialogueInfo::STATUS_CLOSED || $this->current_dialogue->dialogue !== NullNpcDialogue::instance()) {
 			return false;
 		}
-		if(++$this->current_dialogue->tick >= 8){
+		if (++$this->current_dialogue->tick >= 8) {
 			$this->onDialogueClose();
 		}
 		return true;
 	}
 
-	public function handleUpdateAbilities(UpdateAbilitiesPacket $packet) : ?UpdateAbilitiesPacket{
-		if(!$this->modify_abilities){
+	public function handleUpdateAbilities(UpdateAbilitiesPacket $packet): ?UpdateAbilitiesPacket
+	{
+		if (!$this->modify_abilities) {
 			return null;
 		}
 
 		$data = $packet->getData();
 		$ability_layers = $data->getAbilityLayers();
-		foreach($ability_layers as $index => $layer){
+		foreach ($ability_layers as $index => $layer) {
 			$abilities = $layer->getBoolAbilities();
-			if(isset($abilities[AbilitiesLayer::ABILITY_OPERATOR])){
+			if (isset($abilities[AbilitiesLayer::ABILITY_OPERATOR])) {
 				$abilities[AbilitiesLayer::ABILITY_OPERATOR] = false;
-				$ability_layers[$index] = new AbilitiesLayer($layer->getLayerId(), $abilities, $layer->getFlySpeed(), $layer->getWalkSpeed());
+				$ability_layers[$index] = new AbilitiesLayer($layer->getLayerId(), $abilities, $layer->getFlySpeed(), $layer->getVerticalFlySpeed(), $layer->getWalkSpeed());
 			}
 		}
 
@@ -77,38 +81,41 @@ final class PlayerInstance{
 		));
 	}
 
-	public function sendDialogue(NpcDialogue $dialogue, bool $update = false) : void{
-		if($this->current_dialogue !== null && $this->current_dialogue->status !== PlayerNpcDialogueInfo::STATUS_CLOSED){
-			if($update){
+	public function sendDialogue(NpcDialogue $dialogue, bool $update = false): void
+	{
+		if ($this->current_dialogue !== null && $this->current_dialogue->status !== PlayerNpcDialogueInfo::STATUS_CLOSED) {
+			if ($update) {
 				$this->current_dialogue->dialogue = $dialogue;
 				$this->sendDialogueWindow($this->current_dialogue);
-			}else{
+			} else {
 				$this->removeCurrentDialogue();
 				$this->next_dialogue = new PlayerNpcDialogueInfo($this->current_dialogue->actor_runtime_id, $dialogue, PlayerNpcDialogueInfo::STATUS_SENT, 0);
 			}
-		}else{
+		} else {
 			$this->removeCurrentDialogue();
 			$this->current_dialogue = new PlayerNpcDialogueInfo(Entity::nextRuntimeId(), $dialogue, PlayerNpcDialogueInfo::STATUS_SENT, 0);
 			$this->sendDialogueInternal($this->current_dialogue);
 		}
 	}
 
-	private function sendDialogueInternal(PlayerNpcDialogueInfo $info) : void{
+	private function sendDialogueInternal(PlayerNpcDialogueInfo $info): void
+	{
 		$this->logger->debug("Attempting to send dialogue");
 		$session = $this->player->getNetworkSession();
 		$metadata = new EntityMetadataCollection();
 		$metadata->setGenericFlag(EntityMetadataFlags::IMMOBILE, true);
 		$metadata->setByte(EntityMetadataProperties::HAS_NPC_COMPONENT, 1);
-		foreach($info->dialogue->getTexture()->apply($info->actor_runtime_id, $metadata, new Vector3(0.0, -2.0, 0.0)) as $packet){
+		foreach ($info->dialogue->getTexture()->apply($info->actor_runtime_id, $metadata, new Vector3(0.0, -2.0, 0.0)) as $packet) {
 			$session->sendDataPacket($packet);
 		}
 		$this->sendDialogueWindow($info);
 	}
 
-	private function sendDialogueWindow(PlayerNpcDialogueInfo $info) : void{
+	private function sendDialogueWindow(PlayerNpcDialogueInfo $info): void
+	{
 		$session = $this->player->getNetworkSession();
 		$is_op = $this->player->hasPermission(DefaultPermissions::ROOT_OPERATOR);
-		if($is_op){
+		if ($is_op) {
 			$this->modify_abilities = true;
 			$session->syncAbilities($this->player);
 		}
@@ -118,7 +125,7 @@ final class PlayerInstance{
 			$info->dialogue->getText(),
 			(string) $info->actor_runtime_id,
 			$info->dialogue->getName(),
-			json_encode(array_map(static fn(NpcDialogueButton $button) : array => [
+			json_encode(array_map(static fn(NpcDialogueButton $button): array => [
 				"button_name" => $button->getName(),
 				"text" => $button->getText(),
 				"data" => $button->getData(),
@@ -126,22 +133,24 @@ final class PlayerInstance{
 				"type" => $button->getType()
 			], $info->dialogue->getButtons()), JSON_THROW_ON_ERROR)
 		));
-		if($is_op){
+		if ($is_op) {
 			$this->modify_abilities = false;
 			$session->syncAbilities($this->player);
 		}
 	}
 
-	public function onDialogueReceive() : void{
-		if($this->current_dialogue !== null && $this->current_dialogue->dialogue !== NullNpcDialogue::instance()){
+	public function onDialogueReceive(): void
+	{
+		if ($this->current_dialogue !== null && $this->current_dialogue->dialogue !== NullNpcDialogue::instance()) {
 			$this->current_dialogue->status = PlayerNpcDialogueInfo::STATUS_RECEIVED;
 		}
 	}
 
-	public function onDialogueClose() : void{
-		if($this->current_dialogue !== null && $this->current_dialogue->dialogue === NullNpcDialogue::instance()){
+	public function onDialogueClose(): void
+	{
+		if ($this->current_dialogue !== null && $this->current_dialogue->dialogue === NullNpcDialogue::instance()) {
 			$this->current_dialogue->status = PlayerNpcDialogueInfo::STATUS_CLOSED;
-			if($this->next_dialogue !== null){
+			if ($this->next_dialogue !== null) {
 				$this->current_dialogue = $this->next_dialogue;
 				$this->next_dialogue = null;
 				$this->sendDialogueInternal($this->current_dialogue);
@@ -149,28 +158,32 @@ final class PlayerInstance{
 		}
 	}
 
-	public function getCurrentDialogueInfo() : ?PlayerNpcDialogueInfo{
+	public function getCurrentDialogueInfo(): ?PlayerNpcDialogueInfo
+	{
 		$dialogue = $this->current_dialogue?->dialogue;
 		return $dialogue !== NullNpcDialogue::instance() ? $this->current_dialogue : null;
 	}
 
-	public function getCurrentDialogue() : ?NpcDialogue{
+	public function getCurrentDialogue(): ?NpcDialogue
+	{
 		return $this->getCurrentDialogueInfo()?->dialogue;
 	}
 
-	public function onDialogueRespond(string $scene_name, int $index) : void{
+	public function onDialogueRespond(string $scene_name, int $index): void
+	{
 		$info = $this->getCurrentDialogueInfo();
-		if($info !== null && (int) $scene_name === $info->actor_runtime_id){
-			if(isset($info->dialogue->getButtons()[$index])){
+		if ($info !== null && (int) $scene_name === $info->actor_runtime_id) {
+			if (isset($info->dialogue->getButtons()[$index])) {
 				$info->dialogue->onPlayerRespond($this->player, $index);
-			}else{
+			} else {
 				$info->dialogue->onPlayerRespondInvalid($this->player, $index);
 			}
 		}
 	}
 
-	public function removeCurrentDialogue() : ?PlayerNpcDialogueInfo{
-		if($this->current_dialogue === null || $this->current_dialogue->dialogue === NullNpcDialogue::instance()){
+	public function removeCurrentDialogue(): ?PlayerNpcDialogueInfo
+	{
+		if ($this->current_dialogue === null || $this->current_dialogue->dialogue === NullNpcDialogue::instance()) {
 			return null;
 		}
 
